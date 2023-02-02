17142e4173b28ffa5cfd553d13ac49724db0aff7
Junaid Ahmed <112433148+junaidjaan1388@users.noreply.github.com>
2023-02-02 16:39:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://junaidjaan1388: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@github.com/junaidjaan1388/mogenius okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m mogenius &")
+os.system("git clone https://junaidjaan1388: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@github.com/junaidjaan1388/mogenius okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m Telegram &")
